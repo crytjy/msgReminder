755762fcb99a62bffbafa5918f55d820e1db8e4b
cryfzr <13723794923@163.com>
2018-12-18 21:35:01 +0800

    更新注释

--- msgReminder.py
+++ msgReminder.py
@@ -6,8 +6,8 @@ itchat.auto_login(hotReload=True)
 # 登录时向助手推送消息
 itchat.send('Hello, 已开启消息通知！', toUserName='filehelper')
 # 填写你关注的好友备注（此处以小明为例）
-my_friend = itchat.search_friends(name=u'小明')
-fromUserName = my_friend[0]['UserName']
+myFriend = itchat.search_friends(name=u'小明')
+fromUserName = myFriend[0]['UserName']
 # 控制第一次收到信息时需通知
 isCall = True
 
@@ -15,15 +15,15 @@ isCall = True
 # 调用Twilio，电话通知
 def goddessCallMe():
     # twilio SID
-    account_sid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXX"
+    accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXX"
     # twilio token
-    auth_token = "XXXXXXXXXXXXXXXXXXXXX"
+    authToken = "XXXXXXXXXXXXXXXXXXXXX"
     # twilio 申请的号码
     twilioNumber = "XXXXXXXXXXXXX"
     # twilio 验证的手机号
     myNumber = "XXXXXXXXXXXXXXXXX"
 
-    client = Client(account_sid, auth_token)
+    client = Client(accountSid, authToken)
     call = client.calls.create(
         url='http://demo.twilio.com/docs/voice.xml',
         to=myNumber,
@@ -36,7 +36,7 @@ def goddessCallMe():
 
 
 @itchat.msg_register([itchat.content.TEXT, itchat.content.PICTURE, itchat.content.RECORDING, itchat.content.ATTACHMENT, itchat.content.VIDEO])
-def get_msg(msg):
+def getMsg(msg):
     global isCall
     if msg['FromUserName'] == fromUserName and isCall == True:
         goddessCallMe()
